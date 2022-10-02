ba7af73a29441465c41e96463b5502b982bf8cdc
Raymond Chen <raychen358@gmail.com>
2022-10-02 12:30:20 -0700

    Reduce method for total price

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,29 +2,36 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Cart = ({ cart }) => {
+
+    const totalPrice = cart.reduce((price, item) => price + item.quantity * item.price, 0)
+
     return (
         <div>
-            <h1>Cart Items</h1>
-            {cart.length === 0 &&
-                <div className='cart-empty'> Cart is empty </div>
-            }
+            <CartWrap>
+                <h1>Cart</h1>
+
+                {cart.length === 0 &&
+                    <div className='cart-empty'> Cart is empty </div>
+                }
+            </CartWrap>
             <div>
                 {cart.map((item) => (
                     <Card>
                         <div key={item.id} className='cart'>
                             <Wrap>
-                            <img
-                                className='cart-items'
-                                src={item.image}
-                                alt={item.name}
-                            />
-                            <p>{item.name}</p>
+                                <img
+                                    className='cart-items'
+                                    src={item.image}
+                                    alt={item.name}
+                                />
+                                <p>{item.name}</p>
                             </Wrap>
                             <p>{item.quantity}</p>
-                            <p>${item.price}</p>
+                            <p>${item.price * item.quantity}</p>
                         </div>
                     </Card>
                 ))}
+                <h3 className='total-price'>Total: ${totalPrice}</h3>
             </div>
         </div>
     )
@@ -43,4 +50,12 @@ display:flex;
 align-items:center;
 justify-content:space-between;
 width:30rem;
+`
+
+const CartWrap = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+padding:1rem;
 `
