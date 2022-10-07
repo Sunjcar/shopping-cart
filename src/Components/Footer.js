888c59043d3c831bf51a7996dd7f615b34cd1ad8
Raymond Chen <raychen358@gmail.com>
2022-10-06 22:38:50 -0700

    Fixed footer link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
         w-auto p-2 gap-4
         text-white bg-neutral-800'>
             <p className='footer'>Copyright © Ray Chen</p>
-            <div href="https://github.com/sunjcar" target="_blank" className='text-3xl cursor-pointer
+            <a href="https://github.com/sunjcar" target="_blank" rel='noreferrer' className='text-3xl cursor-pointer
              transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:rotate-720'>
                 <FaGithub/>
-            </div>
+            </a>
         </div>
     );
 };
